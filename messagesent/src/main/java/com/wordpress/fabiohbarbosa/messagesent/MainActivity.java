ddd1c2e787393c195d8952789cda905c3ba7d691
Fabio H. G. Barbosa <fhenrique@ciandt.com>
2015-02-12 08:32:04 -0200

    Android

---
+++ messagesent/src/main/java/com/wordpress/fabiohbarbosa/messagesent/MainActivity.java
@@ -0,0 +1,90 @@
+package com.wordpress.fabiohbarbosa.messagesent;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    private EditText phoneNumber;
+    private EditText smsBody;
+    private Button smsManagerBtn;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initializeCalendar();
+
+        phoneNumber = (EditText) findViewById(R.id.phoneNumber);
+        smsBody = (EditText) findViewById(R.id.smsBody);
+
+        smsManagerBtn = (Button) findViewById(R.id.smsManager);
+        smsManagerBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                sendSmsByManager();
+            }
+        });
+
+    }
+
+    public void sendSmsByManager() {
+        try {
+            // Get the default instance of the SmsManager
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNumber.getText().toString(),
+                    null,
+                    smsBody.getText().toString(),
+                    null,
+                    null);
+            Toast.makeText(getApplicationContext(), "Your sms has successfully sent!",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(),"Your sms has failed...",
+                    Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
+    }
+
+    public void initializeCalendar() {
+        CalendarView calendar = (CalendarView) findViewById(R.id.calendar);
+
+        // sets whether to show the week number.
+        calendar.setShowWeekNumber(false);
+
+        // sets the first day of week according to Calendar.
+        // here we set Monday as the first day of the Calendar
+        calendar.setFirstDayOfWeek(2);
+
+        //The background color for the selected week.
+        calendar.setSelectedWeekBackgroundColor(getResources().getColor(R.color.green));
+
+        //sets the color for the dates of an unfocused month.
+        calendar.setUnfocusedMonthDateColor(getResources().getColor(R.color.transparent));
+
+        //sets the color for the separator line between weeks.
+        calendar.setWeekSeparatorLineColor(getResources().getColor(R.color.transparent));
+
+        //sets the color for the vertical bar shown at the beginning and at the end of the selected date.
+        calendar.setSelectedDateVerticalBar(R.color.darkgreen);
+
+        //sets the listener to be notified upon selected date change.
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            //show the selected date as a toast
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
+                Toast.makeText(getApplicationContext(), day + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+}
